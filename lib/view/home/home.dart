a8b188abff4a7dde915f4bf1151329e3835d4605
ahmed <ahmedeltony275@gmail.com>
2023-08-08 11:10:28 +0300

    updated version

--- lib/view/home/home.dart
+++ lib/view/home/home.dart
@@ -13,6 +13,7 @@ class _HomeState extends State<Home> {
     return Scaffold(
       appBar: AppBar(
         title: Text("News"),
+        backgroundColor: Colors.indigoAccent,
         centerTitle: true,
         shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.only(
--- lib/view/tab/news_tab/news_tab.dart
+++ lib/view/tab/news_tab/news_tab.dart
@@ -2,7 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:news_app/data/api/api_manger.dart';
 import 'package:news_app/data/model/SourcesResponseDm.dart';
 
-class NewsTab extends StatelessWidget {
+class NewsTab extends StatefulWidget {
+  @override
+  State<NewsTab> createState() => _NewsTabState();
+}
+
+class _NewsTabState extends State<NewsTab> {
+  int currentIndex = 0;
   @override
   Widget build(BuildContext context) {
     return FutureBuilder<SourcesResponseDm>(
@@ -13,12 +19,51 @@ class NewsTab extends StatelessWidget {
           } else if (snapShot.hasData) {
             return getscreen(snapShot.data!.sources!);
           } else {
-            return CircularProgressIndicator();
+            return Center(child: CircularProgressIndicator());
           }
         });
   }
 
   Widget getscreen(List<Sources> sources) {
-    return Scaffold();
+    return DefaultTabController(
+        length: sources.length,
+        child: Column(
+          children: [
+            SizedBox(
+              height: 12,
+            ),
+            TabBar(
+              tabs: sources.map((source) {
+                return getTab(source, currentIndex == sources.indexOf(source));
+              }).toList(),
+              onTap: (index) {
+                currentIndex = index;
+                setState(() {});
+              },
+              isScrollable: true,
+              indicatorColor: Colors.transparent,
+            ),
+            Expanded(
+              child: TabBarView(
+                children: sources.map((source) {
+                  return Text(source.name ?? "unknown");
+                }).toList(),
+              ),
+            )
+          ],
+        ));
+  }
+
+  Widget getTab(Sources source, bool isSelected) {
+    return Container(
+      padding: EdgeInsets.all(12),
+      decoration: BoxDecoration(
+        color: isSelected == true ? Colors.indigoAccent : Colors.white,
+        borderRadius: BorderRadius.circular(6),
+      ),
+      child: Text(source.name ?? "unkown",
+          style: TextStyle(
+              color: isSelected == true ? Colors.white : Colors.indigoAccent)),
+    );
   }
 }
